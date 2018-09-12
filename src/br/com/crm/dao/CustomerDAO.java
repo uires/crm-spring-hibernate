e820776f585c597ff5d95bddb6fa1ccaa776581d
uires <uiresdeivide1@gmail.com>
2018-09-12 11:47:03 -0300

    classe customer DAO

---
+++ src/br/com/crm/dao/CustomerDAO.java
@@ -0,0 +1,59 @@
+package br.com.crm.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
+import br.com.crm.IF.IFGenericDAO;
+import br.com.crm.entity.Customer;
+import br.com.crm.factory.HibernateFactory;
+
+@Repository
+public class CustomerDAO implements IFGenericDAO<Customer> {
+	private Session session;
+
+	@Override
+	public void save(Customer t) {
+		session = HibernateFactory.getSession();
+		session.beginTransaction();
+		session.save(t);
+		session.getTransaction().commit();
+	}
+
+	@Override
+	public Customer selectById(Long id) {
+		session = HibernateFactory.getSession();
+		session.beginTransaction();
+		Customer customer = session.get(Customer.class, id);
+		session.getTransaction().commit();
+		return customer;
+	}
+
+	@Override
+	public void update(Customer customer) {
+		session = HibernateFactory.getSession();
+		session.beginTransaction();
+		session.update(customer);
+		session.getTransaction().commit();
+	}
+
+	@Override
+	public List<Customer> selectAll() {
+		session = HibernateFactory.getSession();
+		session.beginTransaction();
+		ArrayList<Customer> list = (ArrayList<Customer>) session.createQuery("FROM Customer").getResultList();
+		session.getTransaction().commit();
+		return list;
+	}
+
+	@Override
+	public void delete(Long id) {
+		session = HibernateFactory.getSession();
+		session.beginTransaction();
+		session.delete(this.selectById(id));
+		session.getTransaction().commit();
+	}
+
+}
