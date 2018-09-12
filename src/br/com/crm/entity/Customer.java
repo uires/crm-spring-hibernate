fd7e02797d261426fd17c616e5283bc474a801f9
uires <uiresdeivide1@gmail.com>
2018-09-12 11:47:16 -0300

    entidade customer

---
+++ src/br/com/crm/entity/Customer.java
@@ -0,0 +1,59 @@
+package br.com.crm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "frist_name", nullable = false)
+	private String fristName;
+	@Column(name = "second_name", nullable = false)
+	private String secondName;
+	@Column(name = "email", unique = true)
+	private String email;
+
+	public Customer() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFristName() {
+		return fristName;
+	}
+
+	public void setFristName(String fristName) {
+		this.fristName = fristName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
