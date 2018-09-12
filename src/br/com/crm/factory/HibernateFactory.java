01bd320784eb4e7874283f0428503bf29ef18dc8
uires <uiresdeivide1@gmail.com>
2018-09-11 21:50:55 -0300

    primeiro commit

---
+++ src/br/com/crm/factory/HibernateFactory.java
@@ -0,0 +1,10 @@
+package br.com.crm.factory;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateFactory {
+	public static Session getSession() {
+		return new Configuration().configure().buildSessionFactory().getCurrentSession();
+	}
+}
