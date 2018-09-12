c45755b2e59fa2d8deefb9911b0759ca2169727a
uires <uiresdeivide1@gmail.com>
2018-09-12 10:31:39 -0300

    classes de configuração

--- src/br/com/crm/configuration/ServletConfig.java
+++ src/br/com/crm/configuration/ServletConfig.java
@@ -1,6 +1,22 @@
 package br.com.crm.configuration;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class ServletConfig {
+public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { AppWebConfiguration.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
 
 }
